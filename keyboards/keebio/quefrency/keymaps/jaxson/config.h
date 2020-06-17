d7bde3e1ff6118828a5e26ff83bb68a54ee5e9f6
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-06-16 23:56:11 -0700

    Use NKRO

--- keyboards/keebio/quefrency/keymaps/jaxson/config.h
+++ keyboards/keebio/quefrency/keymaps/jaxson/config.h
@@ -26,6 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL
 // #define USE_I2C
 
+#define BOOTMAGIC_KEY_SALT KC_EQL
+
 #define COMBO_COUNT 2
 
 #define COMBO_TERM 200
