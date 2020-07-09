3f8121e8fb8620cd965bde64fede6242d293e7f5
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-07-08 20:45:20 -0700

    Move layer toggle

--- keyboards/keebio/quefrency/keymaps/jaxson/keymap.c
+++ keyboards/keebio/quefrency/keymaps/jaxson/keymap.c
@@ -33,7 +33,8 @@ enum custom_keycodes {
   BACKSPACE,
   FUNC,
   EXIT,
-  ESCAPE
+  ESCAPE,
+  CANCEL
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -94,6 +95,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       else {
         if (!was_ctrl) unregister_code(KC_ESC);
       }
+      break;
+    }
+    case CANCEL: {
+      if (record->event.pressed) SEND_STRING(SS_LCTL("g"));
       break;
     }
   }
@@ -158,24 +163,24 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5, KC_6,      KC_7,      KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_INSERT, FUNC, \
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5, KC_6,      KC_7,      KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_HOME, KC_END, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T, KC_Y,      KC_U,      KC_I,     KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,         \
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G, KC_H,      KC_J,      KC_K,     KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                   \
-    SHIFT,   KC_Z,    KC_X,    KC_C,    KC_V,     KC_B, KC_N,      KC_M,      KC_COMM,  KC_DOT,  KC_SLSH, KC_UP,   KC_END,                   \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_APP,  KC_SPACE,       BACKSPACE, BACKSPACE, KC_SLSH,  KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT
+    SHIFT,   KC_Z,    KC_X,    KC_C,    KC_V,     KC_B, KC_N,      KC_M,      KC_COMM,  KC_DOT,  KC_SLSH, KC_UP,   FUNC,                     \
+    KC_LCTL, KC_LGUI, KC_LALT, KC_APP,  KC_SPACE,       BACKSPACE, BACKSPACE, CANCEL,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [_LOWER] = LAYOUT(
-    KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,  KC_INSERT, EXIT, \
-    KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,   KC_EQL,  _______,              \
-    TO(_BASE), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______,                       \
-    _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP,   _______,                       \
-    _______,   _______, _______, _______, _______,          _______, _______, _______, _______, KC_HOME, KC_PGDOWN, KC_END
+    KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,  _______, _______, \
+    KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,   KC_EQL,  _______,          \
+    TO(_BASE), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______,                   \
+    _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP,   EXIT,                      \
+    _______,   _______, _______, _______, _______,          _______, _______, _______, _______, _______, KC_PGDOWN, _______
   ),
   [_NUM] = LAYOUT(
-    ESCAPE,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F1,   EXIT, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,       \
-    KC_BSPC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                \
-    KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                \
-    _______, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,           KC_SPC,  KC_SPC,  _______, _______, _______, _______, _______
+    ESCAPE,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F1,   KC_F3, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        \
+    KC_BSPC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                 \
+    KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, EXIT,                    \
+    _______, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,           KC_SPC,  KC_SPC,  KC_SPC,  _______, _______, _______, _______
   )
 };
