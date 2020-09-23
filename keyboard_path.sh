051534616ec321d8e648b046bf11b47b9378da94
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-09-23 15:24:16 -0700

    Add brutal60

--- keyboard_path.sh
+++ keyboard_path.sh
@@ -6,6 +6,8 @@ if [ "$KEY" == "quefrency" ]; then
   STATUS="keebio/quefrency/rev1"
 elif [ "$KEY" == "cospad" ]; then
   STATUS="cospad"
+elif [ "$KEY" == "brutal60" ]; then
+  STATUS="cannonkeys/an_c"
 fi
 
 if [ "$STATUS" == "none" ]; then
