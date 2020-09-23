3b3b345414833f579f7f84871c86c45dad9c4188
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-09-23 15:35:27 -0700

    Update brtual60 keymap

--- keyboards/cannonkeys/an_c/rules.mk
+++ keyboards/cannonkeys/an_c/rules.mk
@@ -12,14 +12,14 @@ DFU_SUFFIX_ARGS = -v FFFF -p FFFF
 #
 
 
-BOOTMAGIC_ENABLE = lite	# Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = full	# Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
 CONSOLE_ENABLE = yes	# Console for debug
 COMMAND_ENABLE = yes    # Commands for debug and configuration
 SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes	    # USB Nkey Rollover
-CUSTOM_MATRIX = no # Custom matrix file
+CUSTOM_MATRIX = no      # Custom matrix file
 BACKLIGHT_ENABLE = yes
 RGBLIGHT_ENABLE = yes
 WS2812_DRIVER = spi
