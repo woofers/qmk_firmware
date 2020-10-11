6340a83ca06d6e40d39e15229b6c25a36fd05bea
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-10-10 22:50:58 -0700

    Cleanup keymap for quefrency

--- keyboards/keebio/quefrency/keymaps/jaxson/keymap.c
+++ keyboards/keebio/quefrency/keymaps/jaxson/keymap.c
@@ -7,8 +7,21 @@
 
 #define has_ctrl() get_mods() & MOD_MASK_CTRL
 
+
+enum custom_keycodes {
+  ESCAPE = SAFE_RANGE,
+  FUNC,
+  TRIM_LINE
+};
+
+
 #define KC_BACK    LCTL(KC_LEFT)
 #define KC_FORWARD LCTL(KC_RIGHT)
+#define KC_UNDO    LCTL(KC_Z)
+#define KC_YANK    LCTL(KC_C)
+#define KC_PASTE   LCTL(LSFT(KC_V))
+#define KC_CUT     TRIM_LINE
+#define KC_FUNC    FUNC
 
 /*
   VENDOR_ID:      0xCB10: 51984
@@ -25,16 +38,10 @@ void matrix_scan_user(void) {
     leading = false;
     leader_end();
     SEQ_ONE_KEY(KC_FORWARD) {
-      SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_RIGHT))));
-      tap_code(KC_BSPC);
+      SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_RIGHT))) SS_TAP(X_BSPC));
     }
   }
 }
-
-enum custom_keycodes {
-  ESCAPE = SAFE_RANGE,
-  FUNC
-};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
@@ -60,6 +67,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       else {
         if (!was_ctrl) unregister_code(KC_ESC);
+      }
+      break;
+    }
+    case TRIM_LINE: {
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_END)) SS_TAP(X_X)));
       }
       break;
     }
@@ -105,15 +118,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-    FUNC,    KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+    KC_FUNC, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______, _______,
-    KC_LCTL, KC_LALT, KC_LALT, _______, KC_SPC,           KC_SPC, KC_SPC,  _______, _______, _______, _______, _______
+    KC_LCTL, _______, KC_LALT, _______, KC_SPC,           KC_SPC, KC_SPC,  _______, _______, _______, _______, _______
   ),
   [_LOWER] = LAYOUT(
-    KC_ESC,  KC_F1,   KC_F2,      KC_F3,      KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL, KC_DEL,
-    _______, _______, KC_FORWARD, _______,    _______, _______, _______, _______, KC_HOME, KC_END,   _______, _______, _______, _______,
-    _______, _______, _______,    _______,    _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, _______,
-    _______, _______, _______,    KC_LEAD,    _______, KC_BACK, _______, _______, _______, _______,  _______, _______, _______,
-    _______, _______, _______,    _______,    _______,          _______, _______, _______, _______,  _______, _______, _______
+    KC_ESC,  KC_F1,   KC_F2,      KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  _______, _______,
+    _______, _______, KC_FORWARD, _______, _______, _______, KC_YANK, KC_UNDO, KC_HOME, KC_END,   KC_PASTE, _______, _______, _______,
+    _______, _______, _______,    KC_CUT,  _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,  _______, _______,
+    _______, _______, KC_DEL,     KC_LEAD, KC_LSFT, KC_BACK, _______, _______, _______, _______,  _______,  _______, _______,
+    _______, _______, _______,    _______, _______,          _______, _______, _______, _______,  _______,  _______, _______
+
   )
 };
