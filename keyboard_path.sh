a55e72a478e74e4473c086b807dffe988f7a22a6
Jaxson Van Doorn <woofers1tt@gmail.com>
2021-03-23 13:49:05 -0700

    Add tofu60

--- keyboard_path.sh
+++ keyboard_path.sh
@@ -8,6 +8,8 @@ elif [ "$KEY" == "cospad" ]; then
   STATUS="cospad"
 elif [ "$KEY" == "brutal60" ]; then
   STATUS="cannonkeys/an_c"
+elif [ "$KEY" == "tofu60" ]; then
+  STATUS="melgeek/mj64/rev3"
 fi
 
 if [ "$STATUS" == "none" ]; then
